bda6bde4893240f1486f3e5452c02833e1a4155a
Dan LaManna <dan.lamanna@kitware.com>
2021-08-31 11:14:32 -0400

    Use pytest-httpserver instead of responses

--- tests/test_session.py
+++ tests/test_session.py
@@ -1,13 +1,34 @@
-import responses
+import pytest
+from werkzeug.wrappers import Response
 
 from retryable_requests.session import RetryableSession
 
+LISTEN_PORT = 6749
 
-@responses.activate
-def test_session_retries():
-    responses.add(responses.GET, 'http://foo.dev', status=500)
-    responses.add(responses.GET, 'http://foo.dev', json={'success': True})
 
-    session = RetryableSession(base_url='http://foo.dev')
-    r = session.get('')
-    assert r.ok, r.data
+@pytest.fixture(scope='session')
+def httpserver_listen_address():
+    return 'localhost', LISTEN_PORT
+
+
+@pytest.fixture
+def base_url(httpserver_listen_address):
+    return f'http://{httpserver_listen_address[0]}:{httpserver_listen_address[1]}'
+
+
+def fails_first_time(request):
+    if fails_first_time.responded:
+        return Response({}, 200)
+    else:
+        fails_first_time.responded = True
+        return Response({}, 500)
+
+
+fails_first_time.responded = False  # type: ignore
+
+
+def test_session_retries_bad_status_codes(httpserver, base_url):
+    httpserver.expect_request('/', method='GET').respond_with_handler(fails_first_time)
+    session = RetryableSession(base_url)
+    r = session.get('/')
+    assert r.ok, r.text
