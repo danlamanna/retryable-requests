a3d3c918116fa9091781e7fe242f9583b6584141
Brian Helba <brian.helba@kitware.com>
2021-11-19 07:45:02 -0500

    Allow pytest-httpserver to choose its own port

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,13 +2,6 @@ import pytest
 
 from retryable_requests import RetryableSession
 
-LISTEN_PORT = 6749
-
-
-@pytest.fixture(scope='session')
-def httpserver_listen_address():
-    return 'localhost', LISTEN_PORT
-
 
 @pytest.fixture
 def session():
