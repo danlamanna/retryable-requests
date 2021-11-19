1f2fcf85478672e5d9e48d1af67216e6263db60d
Brian Helba <brian.helba@kitware.com>
2021-11-19 07:16:04 -0500

    Make "fails_first_time" reset state between test cases

--- tests/test_session.py
+++ tests/test_session.py
@@ -1,3 +1,5 @@
+from itertools import chain, repeat
+
 import pytest
 from requests.exceptions import ConnectionError
 from urllib3.connectionpool import HTTPConnectionPool
@@ -6,24 +8,23 @@ from werkzeug.wrappers import Response
 from retryable_requests.session import DEFAULT_RETRY_STRATEGY
 
 
-def fails_first_time(request):
-    if fails_first_time.responded:
-        return Response({}, 200)
-    else:
-        fails_first_time.responded = True
-        return Response({}, 500)
+@pytest.fixture
+def fails_first_time(mocker):
+    return mocker.Mock(
+        side_effect=chain(
+            [Response({}, 500)],
+            repeat(Response({}, 200)),
+        )
+    )
 
 
-fails_first_time.responded = False  # type: ignore
-
-
-def test_base_url_session_retries_bad_status_codes(httpserver, base_url_session):
+def test_base_url_session_retries_bad_status_codes(httpserver, base_url_session, fails_first_time):
     httpserver.expect_request('/', method='GET').respond_with_handler(fails_first_time)
     r = base_url_session.get('/')
     assert r.ok, r.text
 
 
-def test_session_retries_bad_status_codes(httpserver, base_url, session):
+def test_session_retries_bad_status_codes(httpserver, base_url, session, fails_first_time):
     httpserver.expect_request('/', method='GET').respond_with_handler(fails_first_time)
     r = session.get(f'{base_url}/')
     assert r.ok, r.text
